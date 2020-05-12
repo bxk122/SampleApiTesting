df3efcd719f278150f19fe54e6a797a208189b88
Badal Karki <53354741+bxk122@users.noreply.github.com>
2020-05-12 08:14:28 -0500

    added some comments

--- src/test/java/KarateApiTesting/ValidateResponse.java
+++ src/test/java/KarateApiTesting/ValidateResponse.java
@@ -30,28 +30,35 @@ public class ValidateResponse {
         else {
             results.put("age", "Fail");
         }
-
+        
+        //check first name
         if(p.getName().length() <= 10) {
             results.put("Name", "Pass");
         }
         else {
             results.put("Name", "Fail");
         }
+        
+        //check last name
         if(p.getLast().length() <=10) {
             results.put("Last", "Pass");
         }
         else {
             results.put("Last", "Fail");
         }
+        
+        //check gender
         if(p.getGender() == 'F' || p.getGender() == 'M') {
             results.put("Gender", "Pass");
         }
         else {
             results.put("Gender", "Fail");
         }
+        
+        //print out all the possible result to track pass and fail info 
         System.out.println(results);
 
-
+        //loop to track the no of fail items 
         for (Map.Entry<String, String> entry : results.entrySet()) {
             if(entry.getValue() == "Fail")
             {
@@ -61,6 +68,7 @@ public class ValidateResponse {
         return failCount;
     }
 
+    //method to validate the response time 
     public Boolean validateResponseTime(int responseTime) {
         Boolean checkResponseTime = false;
         if(responseTime < 500) {
